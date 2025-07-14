237a6a23ec82985e61a35de3fe7c321c1d3be3a0
melatfekadu-s <ejetamelat@gmail.com>
2025-07-14 14:31:38 +0300

    basic operation

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+total = number1 + number2
+diff = number2 - number1
+product = number1 * number2
+
+print(f"Addition of 10 and 5 is {total}")
+print(f"Subtraction of 10 and 5 is {diff}")
+print(f"Multiplication of 10 and 5 is {product}")
+
+
+
