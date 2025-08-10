597f87899f527b4462935c7a3a01011103e01b91
melatfekadu-s <ejetamelat@gmail.com>
2025-08-10 16:55:17 +0300

    python

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,29 +1,28 @@
-# library_system.py
-
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __repr__(self):
         return f"Book: {self.title} by {self.author}"
+        
 
 
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self, title, author, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __repr__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
+    def __init__(self, title, author, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __repr__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -31,9 +30,11 @@ class Library:
     def __init__(self):
         self.books = []
 
-    def add_book(self, book):
-        self.books.append(book)
+
+    def add_book(self, books):
+        self.books.append(books)
+
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book.__str__())
